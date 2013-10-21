f608b82f2754a68c7701e3f9b57c8a93c78f194e
Cyril Balit <balit@ekino.com>
2013-10-21 14:06:42 +0200

    step 22

--- app/js/controllers/RestaurantsController.js
+++ app/js/controllers/RestaurantsController.js
@@ -7,12 +7,51 @@ foodMeApp.controller('RestaurantsController',
     $location.url('/customer');
   }
 
-  $scope.restaurants = Restaurant.query();
-
-  $scope.filter = {
+  var filter = $scope.filter = {
     cuisine: [],
     price: null,
     rating: null
+  };
+
+  var allRestaurants = Restaurant.query(filterRestaurants);
+  $scope.$watch('filter', filterRestaurants, true);
+
+  function filterRestaurants() {
+    $scope.restaurants = [];
+
+    // filter
+    angular.forEach(allRestaurants, function(item, key) {
+      if (filter.price && filter.price !== item.price) {
+        return;
+      }
+
+      if (filter.rating && filter.rating !== item.rating) {
+        return;
+      }
+
+      if (filter.cuisine.length && filter.cuisine.indexOf(item.cuisine) === -1) {
+        return;
+      }
+
+      $scope.restaurants.push(item);
+    });
+  };
+
+
+  $scope.CUISINE_OPTIONS = {
+    african: 'African',
+    american: 'American',
+    barbecue: 'Barbecue',
+    cafe: 'Cafe',
+    chinese: 'Chinese',
+    'czech/slovak': 'Czech / Slovak',
+    german: 'German',
+    indian: 'Indian',
+    japanese: 'Japanese',
+    mexican: 'Mexican',
+    pizza: 'Pizza',
+    thai: 'Thai',
+    vegetarian: 'Vegetarian'
   };
 
 });

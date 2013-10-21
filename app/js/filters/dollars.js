dbb9185060b970f7748e74309ab5b60bdeccc889
Cyril Balit <balit@ekino.com>
2013-10-21 14:03:46 +0200

    step 21

--- app/js/filters/dollars.js
+++ app/js/filters/dollars.js
@@ -1,15 +1,15 @@
 'use strict';
 
 foodMeApp.filter('dollars', function() {
-    var DOLLARS = {
-        1: '$',
-        2: '$$',
-        3: '$$$',
-        4: '$$$$',
-        5: '$$$$$'
-    }
+  var DOLLARS = {
+    1: '$',
+    2: '$$',
+    3: '$$$',
+    4: '$$$$',
+    5: '$$$$$'
+  }
 
-    return function(dollarCount) {
-        return DOLLARS[dollarCount]
-    };
+  return function(dollarCount) {
+    return DOLLARS[dollarCount]
+  };
 });
